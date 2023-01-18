953abe23300e7fd09517b9a13d47f797f5850fa5
WhichWho <1366505103@qq.com>
2023-01-19 00:14:50 +0800

    fix gradle.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,14 @@ plugins {
     id ("com.android.library") version "7.4.0" apply false
     id ("org.jetbrains.kotlin.android") version "1.7.0" apply false
 }
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.4.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.0")
+    }
+}
